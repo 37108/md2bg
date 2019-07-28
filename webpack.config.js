c60a0b3a49022979dffadb6eea57c6477cec1c0d
37108 <37108mail@gmail.com>
2019-07-28 21:01:23 +0900

    [fix] fix build tool
    - use webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@ module.exports = {
   mode: 'production',
   resolve: { extensions: ['.ts', '.js'] },
   target: 'node',
+  entry: {
+    'build/handler': './src/index.ts',
+    'build/bin': './src/bin/index.ts',
+  },
+  output: {
+    path: __dirname,
+    filename: '[name].js',
+  },
   module: {
     rules: [
       {
