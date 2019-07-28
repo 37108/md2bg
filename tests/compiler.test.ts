5c172da7a3ed49fd035381f948c8673e2c5c765a
37108 <37108mail@gmail.com>
2019-07-28 19:58:11 +0900

    [feat] add jest
    - add jest configuration
    - add a few test

---
+++ tests/compiler.test.ts
@@ -0,0 +1,17 @@
+import { compiler } from '../src/compiler'
+
+const textNode = {type: 'text', value: 'Alpha bravo charlie.'}
+
+const heading1Node = {
+  type: 'heading',
+  depth: 1,
+  children: [{type: 'text', value: 'Alpha'}]
+}
+
+test('text', () => {
+  expect(compiler['text'](textNode)).toBe('Alpha bravo charlie.')
+})
+
+test('heading1', () => {
+  expect(compiler['heading'](heading1Node)).toBe('* Alpha')
+})
