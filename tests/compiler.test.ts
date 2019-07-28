6ea3afd53aa5435e9d23de3d77a67b259b7d8d3f
37108 <37108mail@gmail.com>
2019-07-28 20:37:26 +0900

    [fix] add test

--- tests/compiler.test.ts
+++ tests/compiler.test.ts
@@ -1,10 +1,14 @@
 import { compiler } from '../src/compiler'
 
 const textNode = {type: 'text', value: 'Alpha bravo charlie.'}
-
 const heading1Node = {
   type: 'heading',
   depth: 1,
+  children: [{type: 'text', value: 'Alpha'}]
+}
+const heading2Node = {
+  type: 'heading',
+  depth: 2,
   children: [{type: 'text', value: 'Alpha'}]
 }
 
@@ -14,4 +18,7 @@ test('text', () => {
 
 test('heading1', () => {
   expect(compiler['heading'](heading1Node)).toBe('* Alpha')
+})
+test('heading2', () => {
+  expect(compiler['heading'](heading2Node)).toBe('** Alpha')
 })
